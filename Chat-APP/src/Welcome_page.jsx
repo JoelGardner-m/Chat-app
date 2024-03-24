ad321ba3ede5ceff7eb15fd2e5a742dc97d80805
JoelGardner-m <162535245+JoelGardner-m@users.noreply.github.com>
2024-03-24 01:45:25 -0400

    Welcome page -layout: complete

--- Chat-APP/src/Welcome_page.jsx
+++ Chat-APP/src/Welcome_page.jsx
@@ -5,9 +5,15 @@ function Welcom_page(){
 
     return  (
         <div>
-            
+            <div>
+                <h1>Welcome username to chat app </h1>
+                <h3>Before quick question what brings you to our app?</h3>
+                <button>Social</button> <button>Business</button> <button>Other</button>
+                <br/><br/>
+                <a><p>skip</p></a>
+            </div>
 
-            
+
         </div>
 
     )

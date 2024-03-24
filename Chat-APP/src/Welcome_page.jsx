850ba0d4a049946cc45473baf203d6df509ea335
JoelGardner-m <162535245+JoelGardner-m@users.noreply.github.com>
2024-03-24 02:00:42 -0400

    Welcome Page - Basic Css : complete

--- Chat-APP/src/Welcome_page.jsx
+++ Chat-APP/src/Welcome_page.jsx
@@ -4,12 +4,18 @@ function Welcom_page(){
 
 
     return  (
-        <div>
-            <div>
-                <h1>Welcome username to chat app </h1>
-                <h3>Before quick question what brings you to our app?</h3>
-                <button>Social</button> <button>Business</button> <button>Other</button>
+
+        <div style={{position:'relative', alignItems:'center', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{textAlign:'center'}}>
+                
+                <h1 style={{marginTop:'20%', fontSize:'40px'}}>Welcome username to chat app </h1>
+                <br/>
+                <h1>what brings you to our app?</h1>
                 <br/><br/>
+                <button style={{display: 'inline', 'background-color':'#1FEDA8', color: '#05001F', borderRadius: 5, padding:10, paddingLeft:30, paddingRight:30 }}><b>Social</b></button> 
+                <button style={{display: 'inline', 'background-color':'#1FEDA8', color: '#05001F', borderRadius: 5, padding:10, paddingLeft:25, paddingRight:25 }}><b>Business</b></button> 
+                <button style={{display: 'inline', 'background-color':'#1FEDA8', color: '#05001F', borderRadius: 5, padding:10, paddingLeft:30, paddingRight:30 }}><b>Other</b></button>
+                <br/><br/><br/><br/>
                 <a><p>skip</p></a>
             </div>
 
